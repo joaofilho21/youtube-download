42145ebed57074f5c3a3887c6d2204350e543053
Matheus Vanzan <vanzan2015@gmail.com>
2020-03-05 13:12:56 -0300

    Cria main.py com as funcoes de download

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-# youtube-download
+from pytube import YouTube, Playlist
 
 
-Download simples de vídeos e playlists do YouTube usando a biblioteca [PyTube](https://github.com/nficano/pytube)
+VIDEO_URL = 'https://www.youtube.com/watch?v=8EJ3zbKTWQ8&list=PLyORnIW1xT6waC0PNjAMj33FdK2ngL_ik'
+PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PLyORnIW1xT6waC0PNjAMj33FdK2ngL_ik'
 
-### Instalação
-```bash
-pipenv install
-```
 
-#### Download de um vídeo
-```python
 def download_video(video_url):
 	yt = YouTube(video_url)
 	video = yt.streams.get_highest_resolution()
 	video.download()
-```
 
-#### Download de uma playlist
-```python
 def download_playlist(playlist_url):
 	playlist = Playlist(playlist_url)
 	for url in playlist:
 		yt = YouTube(url)
 		video = yt.streams.get_highest_resolution()
 		video.download(output_path='playlist')
-```
 
-#### Download do áudio
-```python
 def download_audio(video_url):
 	yt = YouTube(video_url)
 	audio = yt.streams.filter(only_audio=True)[0]
 	audio.download()
-```
+
+
+if __name__ == '__main__':
+	download_video(VIDEO_URL)
+	download_playlist(PLAYLIST_URL)
+	download_audio(VIDEO_URL)
